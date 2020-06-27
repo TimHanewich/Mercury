a68ce777e59b33ee5e63f4543ccedb0d60505e3b
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-06-27 19:06:24 -0400

    i did a lot of things

---
+++ Mercury/Controls/UserControl_MercuryService.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Mercury.Controls
+{
+    public sealed partial class UserControl_MercuryService : UserControl
+    {
+        public UserControl_MercuryService(string service_name, Stream background_image_stream)
+        {
+            InitializeComponent();
+
+            TextBlock_ServiceName.Text = service_name;
+            BitmapImage bmi = new BitmapImage();
+            bmi.SetSource(background_image_stream.AsRandomAccessStream());
+            Image_Background.Source = bmi;
+
+            //Handlers
+            PointerEntered += MouseOver;
+            PointerExited += MouseLeave;
+
+        }
+
+        public void ApplyRecommendedDimensions()
+        {
+            HorizontalAlignment = HorizontalAlignment.Stretch;
+            VerticalAlignment = VerticalAlignment.Stretch;
+            Height = 150;
+        }
+
+        public void MouseOver(object sender, RoutedEventArgs e)
+        {
+            Image_GrayOut.Visibility = Visibility.Collapsed;
+        }
+
+        public void MouseLeave(object sender, RoutedEventArgs e)
+        {
+            Image_GrayOut.Visibility = Visibility.Visible;
+        }
+    }
+}
--- Mercury/Pages/Page_MainMenu.xaml.cs
+++ Mercury/Pages/Page_MainMenu.xaml.cs
@@ -12,6 +12,13 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Popups;
+using Windows.ApplicationModel.Appointments.DataProvider;
+using Windows.ApplicationModel.DataTransfer;
+using TimHanewich.Csv;
+using Mercury.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -28,9 +35,53 @@ namespace Mercury.Pages
 
             //Set background color
             Rectangle_Background.Fill = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 239, 240, 206));
+
+            SetUpServices();
+            LoadVersionNotes();
+            
         }
 
+        public async void LoadVersionNotes()
+        {
+            StorageFile myFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///ReleaseNotes.csv"));
+            string content = await Windows.Storage.FileIO.ReadTextAsync(myFile);
+            CsvFile file = CsvFile.CreateFromCsvFileContent(content);
+            int t = 0;
+            for (t=file.Rows.Count-1;t>0;t--)
+            {
+                string version = file.Rows[t].Values[0];
+                string publish_date = file.Rows[t].Values[1];
+                string notes = file.Rows[t].Values[2];
+                string full = "v" + version + " (" + publish_date + ") " + notes;
 
+                TextBlock tb = new TextBlock();
+                tb.HorizontalAlignment = HorizontalAlignment.Center;
+                tb.VerticalAlignment = VerticalAlignment.Center;
+                tb.Text = full;
+                tb.TextWrapping = TextWrapping.WrapWholeWords;
+                tb.Margin = new Thickness(5);
+                StackPanel_WhatsNew.Children.Add(tb);
+                
+            }
+        }
+
+        public async void SetUpServices()
+        {
+            //Live Market
+            StorageFile img_livemarkets = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Graphics/LiveMarketBoard.jpg"));
+            Stream s1 = await img_livemarkets.OpenStreamForReadAsync();
+            UserControl_MercuryService ms = new UserControl_MercuryService("Live Markets", s1);
+            ms.ApplyRecommendedDimensions();
+            StackPanel_Services.Children.Add(ms);
+
+            //Financials
+            StorageFile img_financials = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Graphics/Financials2.jpg"));
+            Stream s2 = await img_financials.OpenStreamForReadAsync();
+            UserControl_MercuryService ms2 = new UserControl_MercuryService("Financials", s2);
+            ms2.ApplyRecommendedDimensions();
+            StackPanel_Services.Children.Add(ms2);
+
+        }
         
 
 
